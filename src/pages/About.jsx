1f57ad1a44e76420ff8e641d8fdb79b261f6b4c1
Rameez Raja <135865477+contactrameezraja@users.noreply.github.com>
2024-05-07 20:47:06 -0400

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -55,8 +55,8 @@ const About = () => {
         <h3 className='subhead-text'>Work Experience.</h3>
         <div className='mt-5 flex flex-col gap-3 text-slate-500'>
           <p>
-            I've worked with all sorts of companies, leveling up my skills and
-            teaming up with smart people. Here's the rundown:
+            I've worked in many roles over the years from a data capacity,
+            leveling up my skills and teaming up with people from across the world. Here's the rundown:
           </p>
         </div>
 
